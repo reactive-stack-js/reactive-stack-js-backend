b69a7ef7357f635cca61dbbfd3fc18dc9e89323c
predrags <predrags@nvidia.com>
2021-06-15 12:34:46 +0200

    extending workers with init

--- src/functions/initiate.workers.ts
+++ src/functions/initiate.workers.ts
@@ -6,13 +6,20 @@ import * as path from 'path';
 
 import {filter} from 'lodash';
 
+export type WorkerType = {
+	init: () => void;
+	work: () => void;
+};
+
 const initiateWorkers = (folder: string): void => {
 	const fileNames = fs.readdirSync(folder);
 	const files = filter(fileNames, (name) => !fs.lstatSync(path.join(folder, name)).isDirectory());
 	files.forEach((file: string) => {
 		const absoluteFilePath = path.join(folder, file);
-		const work: () => void = require(absoluteFilePath).default;
+		const worker: WorkerType = require(absoluteFilePath).default;
+		const {init, work} = worker;
 		work?.(); // <- if (work) work();
+		init?.(); // <- if (init) init();
 	});
 
 	const folders = filter(fileNames, (name: string) => fs.lstatSync(path.join(folder, name)).isDirectory());
