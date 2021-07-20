7461f27dfe2aee91ab9a949ecdbe660bdfe58ee2
predrags <predrags@nvidia.com>
2021-07-20 12:53:21 +0200

    improving data middleware adding docs

--- src/middleware/data.middleware.map.ts
+++ src/middleware/data.middleware.map.ts
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 
-const criteria = (scope: string, collection: string): string => `${scope}-${collection}`;
+import StoreScopeType from "../_types/store.scope.type";
+
+const criteria = (scope: StoreScopeType, collection: string): string => `${scope}-${collection}`;
 
 export default class DataMiddlewareMap {
-	public static addMiddleware(scope: string, collection: string, processor: Function): void {
+	public static addMiddleware(scope: StoreScopeType, collection: string, processor: Function): void {
 		DataMiddlewareMap._middlewares.set(criteria(scope, collection), processor);
 	}
 
-	public static hasMiddleware(scope: string, collection: string): boolean {
+	public static hasMiddleware(scope: StoreScopeType, collection: string): boolean {
 		return !!DataMiddlewareMap._middlewares.get(criteria(scope, collection));
 	}
 
-	public static getMiddleware(scope: string, collection: string): Function | null {
+	public static getMiddleware(scope: StoreScopeType, collection: string): Function | null {
 		return DataMiddlewareMap._middlewares.get(criteria(scope, collection));
 	}
 
